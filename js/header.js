93d159ebfc6f0cea731af212ae27698b8ef9e003
InnovationMaekawaTatsuya <t_maekawa@innovation.co.jp>
2022-11-07 15:29:34 +0900

    dev SPデザイン

---
+++ js/header.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".sp-header-menu").on("click", function () {
+        $("#sp-header").toggleClass("sp-header");
+    });
+});
+$(function () {
+    $(".checked-sp-header-btn").on("click", function () {
+        $("#sp-header").toggleClass("sp-header");
+    });
+});
