5a316784ef79d20ff1bd07030ee2859e45f0e0b0
InnovationMaekawaTatsuya <t_maekawa@innovation.co.jp>
2022-11-14 11:56:44 +0900

    dev コード修正

--- js/header.js
+++ js/header.js
@@ -1,10 +1,18 @@
+// $(function () {
+//     $(".sp-header-menu").on("click", function () {
 $(function () {
     $(".sp-header-menu").on("click", function () {
-        $("#sp-header").toggleClass("sp-header");
+        $("#before-header").addClass("none");
+        $(".sp-header").css('background-color', 'transparent');
+        $("#hamburger-place").addClass("hamburger-menu");
+        $("#sp-header").addClass("checked-sp-header-menu");
+        $(".hamburger-none").css('top', '0');
     });
 });
 $(function () {
-    $(".checked-sp-header-btn").on("click", function () {
-        $("#sp-header").toggleClass("sp-header");
+    $(".checked-sp-header-menu").on("click", function () {
+        $("#before-header").removeClass("none");
+        $(".sp-header").css('background-color', 'white');
+        $(".hamburger-none").css('top', '-100%');
     });
 });
